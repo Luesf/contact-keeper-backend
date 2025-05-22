6d60250326acee01cbcb16b1a053a52c95f01cc3
Luis Espinoza <lui.esp.11@gmail.com>
2025-05-21 20:26:09 -0600

    updated deployed backend routes to Render

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import clientRoutes from './routes/clientRoute.js';
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(cors({origin: "https://main.d2lbwrgwhmgvyc.amplifyapp.com/"}));
+app.use(cors({origin: "https://contact-keeper-frontend-f0wo.onrender.com/"}));
 app.use(express.json());
 app.use("/api", clientRoutes)
 app.get("/", (req, res) => {
